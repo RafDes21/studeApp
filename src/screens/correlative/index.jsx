e1ec658843b5bb92965720a8391c58e2436fb51f
unknown <israeltineo84@gmail.com>
2023-05-17 18:04:26 -0300

    [features] styles correlatives

--- src/screens/correlative/index.jsx
+++ src/screens/correlative/index.jsx
@@ -7,8 +7,15 @@ const Correlative = ({ route }) => {
   const { id, itemId } = route.params;
   const subject = CORRELATIVES.find((item) => item.id === id);
   const forStudy = subject.correlatives[itemId].study;
+  const forYield = subject.correlatives[itemId].yield;
+
+  const name = subject.subjects[itemId];
 
   const forStudyArray = forStudy.map((subject, index) => ({
+    id: `${index}`,
+    title: subject,
+  }));
+  const forYieldArray = forYield.map((subject, index) => ({
     id: `${index}`,
     title: subject,
   }));
@@ -22,23 +29,53 @@ const Correlative = ({ route }) => {
 
   const renderContent = () => {
     if (forStudy.length === 0) {
-      return <Text style={styles.emptyText}>No hay nada</Text>;
+      return (
+        <View style={styles.textContainer}>
+          <Text style={styles.emptyText}>No tiene correlativa</Text>
+        </View>
+      );
     } else {
       return (
-        <FlatList
-          style={styles.listCorrelatives}
-          data={forStudyArray}
-          renderItem={renderStudy}
-          keyExtractor={(item) => item.id}
-        />
+        <View style={styles.listCorrelatives}>
+          <FlatList
+            style={styles.flatList}
+            data={forStudyArray}
+            renderItem={renderStudy}
+            keyExtractor={(item) => item.id}
+          />
+        </View>
+      );
+    }
+  };
+
+  const renderContent2 = () => {
+    if (forYield.length === 0) {
+      return (
+        <View style={styles.textContainer}>
+          <Text style={styles.emptyText}>No tiene correlativa</Text>
+        </View>
+      );
+    } else {
+      return (
+        <View style={styles.listCorrelatives}>
+          <FlatList
+            style={styles.flatList}
+            data={forYieldArray}
+            renderItem={renderStudy}
+            keyExtractor={(item) => item.id}
+          />
+        </View>
       );
     }
   };
 
   return (
-    <View>
-      <Text>Para Cursar</Text>
-      <View>{renderContent()}</View>
+    <View style={styles.container}>
+      <Text style={styles.title}>{name}</Text>
+      <Text style={styles.subTitle}>Para Cursar</Text>
+      {renderContent()}
+      <Text style={styles.subTitle}>Para Rendir</Text>
+      {renderContent2()}
     </View>
   );
 };
--- src/screens/correlative/styles.js
+++ src/screens/correlative/styles.js
@@ -1,13 +1,47 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-    listCorrelatives:{
-        backgroundColor:"#0077b6",
-
-    },
-    itemList:{
-       flexDirection:"row",
-       justifyContent:"space-around",
-       paddingVertical:20,
-    }
-})
+  container: {
+    flex: 1,
+    backgroundColor: "#caf0f8",
+  },
+  listCorrelatives: {
+    alignItems: "center",
+  },
+  itemList: {
+    flexDirection: "row",
+    justifyContent:"space-between",
+    paddingHorizontal: 20,
+    paddingVertical: 20,
+   
+  },
+  flatList: {
+    width: "80%",
+    backgroundColor: "#fff",
+    borderRadius:5,
+  },
+  subTitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+    textAlign: "center",
+    paddingVertical: 10,
+    marginTop: 20,
+  },
+  emptyText: {
+    backgroundColor: "#fff",
+    paddingVertical: 50,
+    textAlign: "center",
+    fontSize: 16,
+    width: "80%",
+    borderRadius: 8,
+  },
+  title: {
+    fontSize: 24,
+    textAlign: "center",
+    paddingVertical: 20,
+    
+  },
+  textContainer: {
+    alignItems: "center",
+  },
+});
